01b9d1015b65b4d056d27fe0dfcb2c56b80375bd
Martin Davis <martin_davis3@yahoo.ca>
2020-03-30 15:36:55 -0600

    change class to Pascal Case

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 //Class constructor
-class personalBudget {
+class PersonalBudget {
     constructor ( name, age, budget ) {
         this.name = name;
         this.age = age;
@@ -12,16 +12,24 @@ class personalBudget {
 
 }
 
+
 const getBudgets = ( ...budgetObjs ) => {
     var budgetSum = 0;
     for ( let i = 0; i < budgetObjs.length; i++) {
-        console.log( budgetObjs[i] );
         var budgetVal = budgetObjs[i].budget;
         budgetSum += budgetVal;
-        console.log( budgetSum, budgetVal);
     }
     return budgetSum;    
 }
+
+var johnBudget = new PersonalBudget( 'John', 25, 3000 );
+var jillBudget = new PersonalBudget( 'Jill', 47, 5650 );
+var jeffBudget = new PersonalBudget( 'Jeff', 68, 1625 );
+
+console.log( johnBudget );
+console.log( jillBudget );
+console.log( jeffBudget );
+console.log( getBudgets( johnBudget, jillBudget, jeffBudget) );
 
 
 
